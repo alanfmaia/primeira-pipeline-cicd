0d186c5a7c8859a85e354996894bd7601a3484d5
alanfmaia <72258721+alanfmaia@users.noreply.github.com>
2025-10-21 21:05:49 -0300

    add v2

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo via github actions 2.0")
+	fmt.Fprintf(w, "Aplicacao exemplo via github actions v2.0")
 }
 
 func main() {
